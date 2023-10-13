e390b1f1cec72e518c019a9b8449d185b3b96b36
emirdnz <emirdeniz997@gmail.com>
2023-10-13 22:10:14 +0300

    Migration Yapıldı

---
+++ Hospital.Data/Migrations/20231013190723_HospitalMigration.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class HospitalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Branches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BranchName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 109, DateTimeKind.Local).AddTicks(9735))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Branches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PatientSurname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TcNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GSM = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 110, DateTimeKind.Local).AddTicks(5877))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roller",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 110, DateTimeKind.Local).AddTicks(7851))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roller", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 110, DateTimeKind.Local).AddTicks(9732))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Secretary",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SecretaryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SecretarySurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonalCode = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 112, DateTimeKind.Local).AddTicks(1591))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Secretary", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoctorName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DoctorSurname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Branch = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonalCode = table.Column<int>(type: "int", nullable: false),
+                    GSM = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BranchId = table.Column<int>(type: "int", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 110, DateTimeKind.Local).AddTicks(1860))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Doctors_Branches_BranchId",
+                        column: x => x.BranchId,
+                        principalTable: "Branches",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menuler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MenuAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Area = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Controller = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Css = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RoleID = table.Column<int>(type: "int", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 110, DateTimeKind.Local).AddTicks(3944))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menuler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Menuler_Roller_RoleID",
+                        column: x => x.RoleID,
+                        principalTable: "Roller",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TcNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    GSM = table.Column<int>(type: "int", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 112, DateTimeKind.Local).AddTicks(3646))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Roller_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roller",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Apointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppointmentTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    SecretaryId = table.Column<int>(type: "int", nullable: true),
+                    UsersId = table.Column<int>(type: "int", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 10, 13, 22, 7, 23, 109, DateTimeKind.Local).AddTicks(5147))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Apointments_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Apointments_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Apointments_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Apointments_Secretary_SecretaryId",
+                        column: x => x.SecretaryId,
+                        principalTable: "Secretary",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Apointments_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_DoctorId_RoomId_PatientId_AppointmentTime",
+                table: "Apointments",
+                columns: new[] { "DoctorId", "RoomId", "PatientId", "AppointmentTime" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_Id",
+                table: "Apointments",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_PatientId",
+                table: "Apointments",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_RoomId",
+                table: "Apointments",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_SecretaryId",
+                table: "Apointments",
+                column: "SecretaryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_UsersId",
+                table: "Apointments",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Branches_Id",
+                table: "Branches",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_BranchId",
+                table: "Doctors",
+                column: "BranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_Id",
+                table: "Doctors",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menuler_Id",
+                table: "Menuler",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menuler_RoleID",
+                table: "Menuler",
+                column: "RoleID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_Id",
+                table: "Patients",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roller_Id",
+                table: "Roller",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_Id",
+                table: "Rooms",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Secretary_Id",
+                table: "Secretary",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Id",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Apointments");
+
+            migrationBuilder.DropTable(
+                name: "Menuler");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Secretary");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Branches");
+
+            migrationBuilder.DropTable(
+                name: "Roller");
+        }
+    }
+}
